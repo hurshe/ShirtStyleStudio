5589f825c994101f2415359c4db6a790b848b61b
Robert Khurshudyan <roberthurshudyan@gmail.com>
2023-09-27 20:20:05 +0200

    Fix settings

--- ShirtStyleStudio/settings.py
+++ ShirtStyleStudio/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w-t5((dyrn6%r49_$&x#kj$^#ndwb*i@yuveu%ceg^0n^9gsmv"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
