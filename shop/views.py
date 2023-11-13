a01d20c0b7d07cea864bf00163d9bf518a7b620d
Robert Khurshudyan <roberthurshudyan@gmail.com>
2023-11-13 05:13:31 +0100

    Create page with offers

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import Offer, Product
@@ -5,7 +6,7 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import DeleteView, ListView
-from .forms import OfferUpdateForm, ProductUpdateForm, ProductDeleteForm, CreateProductForm
+from .forms import OfferUpdateForm, ProductUpdateForm, ProductDeleteForm, CreateProductForm, OfferForm
 from django.db.models import Q
 
 
@@ -25,7 +26,17 @@ class OfferCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class OfferReadView(View):
+class OfferDetailReadView(View):
+    template_name = 'shop/offer_detail.html'
+    form_class = OfferForm
+
+    def get(self, request, offer_id):
+        offer = get_object_or_404(Offer, id=offer_id)
+        form = self.form_class(instance=offer)
+        return render(request, self.template_name, {'form': form, 'offer': offer})
+
+
+class AllOffersReadView(View):
 
     def get_queryset(self):
         if self.request.user.is_superuser:
